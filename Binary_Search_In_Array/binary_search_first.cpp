029563ac1fd544604743f76ebd0b7d0c29aefdd5
ankesh102 <ankesh.pandey8@gmail.com>
2023-05-29 00:27:51 +0530

    INT_MAX_Error_Resolved

--- Binary_Search_In_Array/binary_search_first.cpp
+++ Binary_Search_In_Array/binary_search_first.cpp
@@ -26,7 +26,7 @@ int binarySearch(int arr[], int size, int key)
         {
             end = mid - 1;
         } 
-        mid = (start + end) / 2;
+        mid = mid = start + ((end - start) / 2) ;
     }
     return -1; // if element not found case present 
 }
